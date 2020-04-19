16881f69b6a3da6cc04c66a00c29f95b35f2f225
Ryan Raymond <ryan@iMac.local>
2020-04-19 10:53:30 -0400

    finished basic cardlist card and search box components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, {Component} from 'react';
+
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/searchbox/searchbox.component'
+
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
+      searchField: '',
       monsters: []
     }
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response =>response.json())
+      .then(response => response.json())
       .then(users => this.setState({monsters: users}))
   }
-  
+
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className="App">
-      {
-        this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-      }
+        <SearchBox 
+          placeholder='Search for monsters'
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
