03095586eab0891bd4d28c56a9a192a7bc21535b
Ryan Raymond <ryan@iMac.local>
2020-04-19 11:17:09 -0400

    Updates

---
+++ precache-manifest.d488fbd512a3eb54a1d33e9a5fd2f2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33bfd3620eec0fe437d83b285de63359",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "88806df48cf0619e7a69",
+    "url": "/monstersrolodex/static/css/main.79c45304.chunk.css"
+  },
+  {
+    "revision": "a5743e805e8ac7aa158f",
+    "url": "/monstersrolodex/static/js/2.1ea8f631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.1ea8f631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88806df48cf0619e7a69",
+    "url": "/monstersrolodex/static/js/main.163ce14e.chunk.js"
+  },
+  {
+    "revision": "6313ca53dd5a22b04c90",
+    "url": "/monstersrolodex/static/js/runtime-main.1579ee48.js"
+  }
+]);
